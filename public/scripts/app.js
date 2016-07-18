cf474956529c0517b481c3be71b35155240e9e21
Jonathan Wong <jkwong5@gmail.com>
2016-07-17 17:18:40 -0700

    adding filters

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,38 @@
 
   var Cars = function(instance) {
     this.dt = instance.time;
+    this.img_url = instance.img_url;
     this.lic_plate = instance.lic_plate;
     this.lic_state = instance.lic_state;
     this.make = instance.make;
     this.model = instance.model;
   };
 
-  Cars.getData = function(ctx, next) {
-    $.getJSON('/db/invaders', function (data) {
-      data.forEach(function(car) {
-        var cars = new Cars(car);
-      });
-    }).done(function(data) {
-      Cars.appendToBlog(data);
+  Cars.all = [];
+
+  Cars.loadAll = function(data) {
+    Cars.all = [];
+    data.forEach(function(ele) {
+      Cars.all.push(new Cars(ele));
     });
+  };
+
+  Cars.getData = function(ctx, next) {
+    if (localStorage.invaders) {
+      Cars.loadAll(JSON.parse(localStorage.invaders));
+      Cars.appendToBlog(Cars.all);
+    }
+    else {
+      $.getJSON('/db/invaders', function (data) {
+        data.forEach(function(car) {
+          var cars = new Cars(car);
+          var stringData = JSON.stringify(data);
+          localStorage.setItem('invaders', stringData);
+        });
+      }).done(function(data) {
+        Cars.appendToBlog(data);
+      });
+    };
   };
 
   Cars.appendToBlog = function(data) {
