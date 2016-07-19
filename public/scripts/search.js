48b0b89b9289488a8f12df26bc5cbda72b536820
Jonathan Wong <jkwong5@gmail.com>
2016-07-18 21:38:28 -0700

    search js

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,28 +1,33 @@
 (function(module){
 
-var Search = function(instance){
-  this.state = $('#searchState').value;
-  this.plate = $('#searchPlate').value;
-  this.zip = $('#searchZip').value;
-};
+  var Search = function(instance){
+    this.state = instance.state;
+    this.plate = instance.plate;
+    this.zip = instance.zip;
+  };
 
-Search.all = [];
+  Search.all = [];
 
-Search.form = function(){
-  $('#searchForm').on('submit', function(e) {
-    e.preventDefault();
-  }
+  Search.form = function(){
+    $('#searchForm').on('submit', function(e) {
+    });
+  };
 
-Search.state = function(){
-  $('#searchBtn').on('submit', function(){
-    if ($('#searchState').val && $('#searchPlate')){
-      $('#searchResults').hide();
-      $('#blogData').find('li').value() === state;
-      $('#searchResults').append(template())
-  }
-  $('#blogData').find('li').value() === $('')
+  Search.state = function(){
+    $('#searchBtn').on('submit', function(e){
+      if ($('#searchState').val && $('#searchPlate').val){
+        var state = $('#searchState').val;
+        var plate = $('#searchPlate').val;
+        $('#searchResults').hide();
+        var result = $.grep(localStorage.invaders, function(n, index){
+          console.log(state === localStorage.invaders.lic_state);
+        });
+        // $('#searchResults').find('li').val() === state;
+        $('#searchResults').append(result);
+      }
+    // $('#blogData').find('li').val() === $('')
 
-})
-
+    });
+  };
 
 })(window);
