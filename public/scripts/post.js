bc74ab1475eb813dddddb66dad4cf981d93e27b1
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-07-19 20:47:46 -0700

    post func live

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -6,16 +6,22 @@
     $('#imageWidget').hide();
   };
 
-  Post.submitInvader = function () {
-    $('#submitBtn').on('click', function (e) {
+  Post.submitInvader = function() {
+    $('#submitBtn').on('click', function(e) {
       e.preventDefault();
-      var lic_state = $('#stateField').val();
-      var lic_plate = $('#plateField').val();
-      var make = $('#makeField').val();
-      var model = $('#modelField').val();
-      window.location.assign("https://latfa.herokuapp.com/") //Jeff, you're up!
+      var ls = $('#stateField').val();
+      console.log(ls);
+      var lp = $('#plateField').val();
+      console.log(lp);
+      var ma = $('#makeField').val();
+      console.log(ma);
+      var mo = $('#modelField').val();
+      console.log(mo);
+      window.location.assign('/postNew' + '?lp=' + lp + '&ls=' + ls + '&ma=' + ma + '&mo=' + mo);
+      $('#imageWidget').show();
     });
   };
   module.Post = Post;
+  Post.submitInvader();
 
 })(window);
