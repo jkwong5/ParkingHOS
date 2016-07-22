aa8cb9b88595b0ff89170b5c68eda8657615ff8b
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-07-21 18:54:22 -0700

    fixed filter pop

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,7 +1,7 @@
 (function() {
   page('/', index.renderHomePage, Cars.getData);
   page('/postNew/post', index.renderPostPage, index.renderUploadButton, Filters.populatePostModel);
-  page('/search', index.renderSearchPage, Search.clearSearch);
+  page('/search', index.renderSearchPage, Search.populateFilters, Search.clearSearch);
   page('/leaderboard', index.renderLeaderPage);
   page('*', function() {
     console.log('not found');
