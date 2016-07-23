ae699fe1b2041cc9077dc38b589ff9861b10020c
jisenber <isenberg.jacob@yahoo.com>
2016-07-23 12:23:18 -0700

    files cleaned up and submit image added

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -2,7 +2,6 @@
   page('/', Cars.getData, index.renderHomePage);
   page('/postNew/post', index.renderPostPage, index.renderUploadButton, Filters.populatePostModel);
   page('/search', index.renderSearchPage, Search.populateFilters, Search.clearSearch);
-  page('/leaderboard', index.renderLeaderPage);
   page('*', function() {
     console.log('not found');
   });
