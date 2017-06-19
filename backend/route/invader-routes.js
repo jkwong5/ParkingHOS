654fcf9907e0158ec8c6268e0dcef8d115eaa903
jisenber <isenberg.jacob@yahoo.com>
2017-06-18 23:35:47 -0700

    made driver model and route logic that keeps track of repeat offenders

--- backend/route/invader-routes.js
+++ backend/route/invader-routes.js
@@ -2,10 +2,11 @@
 // app modules
 let Router = require('express').Router;
 let Invader = require('../model/invaders.js');
+let Driver = require('../model/drivers.js');
 let createError = require('http-errors');
 let jsonParser = require('body-parser').json();
 let nunjucks = require('nunjucks');
-let path = require('path')
+let path = require('path');
 let fs = require('fs');
 
 // module constants
@@ -14,7 +15,16 @@ let router = module.exports = new Router();
 //submits an invader to mongoDB
 router.post('/submit', jsonParser, (req, res, next) => {
   new Invader(req.body).save()
-  .then(invader => res.json(invader))
+  .then(invader => {
+    let pltAndState = invader.lic_plate.concat(invader.lic_state);
+    let query = {plateAndState: pltAndState};
+    Driver.findOneAndUpdate(query,
+      { '$push': { 'parkingInstances': invader._id } },
+      {upsert:true}, function(err, doc) {
+        if (err) return res.send(500, { error: err });
+        res.json(doc);
+      });
+  })
   .catch(next);
 });
 
--- backend/route/search-routes.js
+++ backend/route/search-routes.js
@@ -2,13 +2,13 @@
 
 // app modules
 let Router = require('express').Router;
-let Invaders = require('../model/invaders.js');
+let Invader = require('../model/invaders.js');
 
 let router = module.exports = new Router();
 
-//will return an invader based on search terms input into the fields
+//TODO: revisit this piece of code now that we have a driver model
 router.get('/search/:lic_state/:lic_plate', (req, res) => {
-  Invaders.find({lic_state: req.params.lic_state, lic_plate: req.params.lic_plate})
+  Invader.find({lic_state: req.params.lic_state, lic_plate: req.params.lic_plate})
   .then(invader => res.json(invader))
   .catch(err => {
     console.error(err);
