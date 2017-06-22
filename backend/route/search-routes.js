33ce6c99795a3e2dc5afe136f2e83f06e2c196ec
jisenber <isenberg.jacob@yahoo.com>
2017-06-21 23:06:59 -0700

    search functionality implemented. next step will be to make not found notification more UI friendly

--- backend/route/search-routes.js
+++ backend/route/search-routes.js
@@ -3,14 +3,39 @@
 // app modules
 let Router = require('express').Router;
 let Invader = require('../model/invaders.js');
+let nunjucks = require('nunjucks');
 
 let router = module.exports = new Router();
 
-//TODO: revisit this piece of code now that we have a driver model
-router.get('/search/:lic_state/:lic_plate', (req, res) => {
-  Invader.find({lic_state: req.params.lic_state, lic_plate: req.params.lic_plate})
-  .then(invader => res.json(invader))
-  .catch(err => {
-    console.error(err);
+router.get('/search/license/:lic_plate', (req, res) => {
+  Invader.find({lic_plate: req.params.lic_plate})
+  .then(invaders => {
+    if(!invaders.length) {
+      res.send('not found');
+    }
+    let renderedInvaders = nunjucks.render('invaders.njk', {invaderList: invaders});
+    res.send(renderedInvaders);
+  });
+});
+
+router.get('/search/state/:stateName', (req, res) => {
+  Invader.find({lic_state: req.params.stateName})
+  .then(invaders => {
+    if(!invaders.length) {
+      res.send('not found');
+    }
+    let renderedInvaders = nunjucks.render('invaders.njk', {invaderList: invaders});
+    res.send(renderedInvaders);
+  });
+});
+
+router.get('/search/:make/:model', (req, res) => {
+  Invader.find({make: req.params.make, model: req.params.model})
+  .then(invaders => {
+    if(!invaders.length) {
+      res.send('not found');
+    }
+    let renderedInvaders = nunjucks.render('invaders.njk', {invaderList: invaders});
+    res.send(renderedInvaders);
   });
 });
--- public/js/invader-search.js
+++ public/js/invader-search.js
@@ -1,52 +1,13 @@
 
-(function(module) {
+(function() {
 
   // let __API_URL__ = 'https://parking-hall-of-shame.herokuapp.com';
 
   let __API_URL__ = 'http://localhost:3000';
 
-  let pic_url;
   let carMake;
   let carModel;
-  let lic_plate;
   let lic_state;
-
-//drop-down menu for list of US states
-  $('#search').on('click', function(e) {
-    e.preventDefault();
-    $('#stateSearch').empty();
-    $.ajax({
-      method: 'GET',
-      url: `${__API_URL__}/states`
-    }).done(function(stateNames) {
-      $('#stateSearch').append(stateNames);
-    });
-  });
-
-
-  //car make dropdown menu
-  $('#panel11').on('click', function() {
-    e.preventDefault();
-    $('#makeSearch').empty();
-    $.ajax ({
-      method: 'GET',
-      url: `${__API_URL__}/cars`
-    }).done(function(carMakes) {
-      $('#makeSearch').append(carMakes);
-    });
-  });
-
-  //populates the drop down for models
-  $('#makeSearch').on('change', function() {
-    let carMake = $('#makeSearch').val();
-    $('#modelSearch').empty();
-    $.ajax({
-      method: 'GET',
-      url: `${__API_URL__}/cars/${carMake}`
-    }).done(function(carModels) {
-      $('#modelSearch').append(carModels);
-    });
-  });
 
 //updates the value of the carMake variable
   $('#makeSearch').on('change', function() {
@@ -58,30 +19,60 @@
   });
 
 //updates value of the license plate state
-  $('#stateSearch').on('change', function() {
-    lic_state = $('#stateSearch').val();
+  $('#searchState').on('change', function() {
+    lic_state = $('#searchState').val();
   });
 
-//ajax post request, at the moment just sends cloudinary url which will be grabbed in a template
-  $('#searchStateBtn').on('submit', function(e) {
-    e.preventDefault();
-    lic_plate = $('#lic_plate_search').val();
+  //search by license plate
+  $('#searchPlateBtn').on('click', function() {
+    let lic_plate_val = $('#lic_plate_search').val();
     $.ajax ({
       type: 'GET',
-      url: `${__API_URL__}/search`,
-      data: JSON.stringify({
-        'lic_plate': lic_plate,
-        'lic_state': lic_state,
-        'make': carMake,
-        'model': carModel}),
-      contentType:'application/json; charset=utf-8',
-      dataType:'json'
-    }).done(function() {
-      console.log('success');
-      $('#lic_plate_search').val('');
-      $('#makeBar').empty();
-      $('#modalInputForm').modal('toggle');
+      url: `${__API_URL__}/search/license/${lic_plate_val}`
+    }).done(function(invaders) {
+      if(invaders === 'not found') {
+        alert('not found');
+        return;
+      } else {
+        $('#modalRRForm').modal('toggle');
+        $('#invaderList').empty();
+        $('#invaderList').append(invaders);
+      }
     });
   });
 
-})(window);
+  //search by state
+  $('#searchStateBtn').on('click', function() {
+    $.ajax ({
+      type: 'GET',
+      url: `${__API_URL__}/search/state/${lic_state}`
+    }).done(function(invaders) {
+      if(invaders === 'not found') {
+        alert('not found');
+        return;
+      } else {
+        $('#modalRRForm').modal('toggle');
+        $('#invaderList').empty();
+        $('#invaderList').append(invaders);
+      }
+    });
+  });
+
+  //search by Make and Model
+  $('#searchModelBtn').on('click', function() {
+    $.ajax ({
+      type: 'GET',
+      url: `${__API_URL__}/search/${carMake}/${carModel}`
+    }).done(function(invaders) {
+      if(invaders === 'not found') {
+        alert('not found');
+        return;
+      } else {
+        $('#modalRRForm').modal('toggle');
+        $('#invaderList').empty();
+        $('#invaderList').append(invaders);
+      }
+    });
+  });
+
+})();
